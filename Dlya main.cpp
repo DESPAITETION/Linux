485aa28eb71c253beb89a11d1bfd1ff0b84711e8
DESPAITETION <hakahu1608@gmail.com>
2025-10-23 17:03:55 +0300

    The fourth task

--- Dlya main.cpp
+++ Dlya main.cpp
@@ -1,6 +1,16 @@
 ﻿#include<iostream>
 #include<string>
+#include<fstream>
 using namespace std;
+void File(string a)
+{
+	ofstream file("C:\\Users\\Пользователь\\OneDrive\\Desktop\\kubsh_history.txt", ios::app);
+	if (file.is_open())
+	{
+		file << a<<endl;
+		file.close();
+	}
+}
 int main()
 {
 	cout << unitbuf;
@@ -10,12 +20,17 @@ int main()
 	{
 		cout << "$ ";
 		getline(cin, input);
-		if (input == "\\q")
+		string result = "$ " + input;
+		File(result);
+		if (input=="\\q")
 		{
 			cout << "End" << endl;
+<<<<<<< HEAD
+=======
+			File("End");
+>>>>>>> a247c3c (The fourth task)
 			break;
 		}
-
 	}
 	
 }
